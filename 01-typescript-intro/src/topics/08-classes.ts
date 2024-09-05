10baa12a88e5a0daaedcd50d6a52776ef1fac28b
Ramón Folguera <ramon.folguera@mobilecashout.com>
2024-09-05 16:38:16 +0200

    classes extension

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -19,9 +19,20 @@ class Person {
     ) {}
 }
 
+export class Hero extends Person {
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string
+    ) {
+        // funcion especial que indica a typescript que tiene que llamar al constructor del padre
+        super( realName, 'New York' )
+    }
+}
 
 // const ironman = new Person('Ironman', 'New York');
-const ironman = new Person('Ironman');
+// const ironman = new Person('Ironman');
+const ironman = new Hero('Ironman', 45, 'Tony');
 
 
 console.log(ironman);
